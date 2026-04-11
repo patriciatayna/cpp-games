c8414daf6d65fc8807005c41ab5cb12dd4bedddf
Patrícia Tayná <69331672+patriciatayna@users.noreply.github.com>
2026-04-11 16:31:25 -0300

    Add files via upload
    commit inicial

---
+++ Book Demonstrations/Code0003/main.cpp
@@ -0,0 +1,12 @@
+// Game Over 3.0 
+// demonstrates an using directive 2.0
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout <<"Game Over!"<< endl;
+    return 0;
+}

27b84925fa9066fb581ab8f33800a53bfde74040
Patrícia Tayná <69331672+patriciatayna@users.noreply.github.com>
2026-04-14 17:04:22 -0300

    Add constructor and greet method to Critter class

--- Book Demonstrations/POO/08 Classes: Critter Caretaker/constructor_critter.cpp
+++ Book Demonstrations/POO/08 Classes: Critter Caretaker/constructor_critter.cpp
@@ -4,22 +4,30 @@
 #include <iostream>
 using namespace std;
 
-class Critter 
+class Critter
 {
-
-}
+    public:
+        int m_Hunger;
+        
+    Critter(int hunger = 0);        // Constructor prototype
+    void Greet();
+};
 
 Critter::Critter(int hunger)
 {
-
+    cout << "A new critter has been born!\n" << endl;
+    m_Hunger = hunger;
 }
 
 void Critter::Greet()
 {
-  
+    cout << "Hi! I'm a critter. My hunger level is " << m_Hunger << ".\n\n";
 }
 
 int main()
 {
-  return 0;
+    Critter crit(7);
+    crit.Greet();
+    
+	return 0;
 }

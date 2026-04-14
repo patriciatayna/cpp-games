b2cc130a949128ec8b67035bc721d700f8270293
Patrícia Tayná <69331672+patriciatayna@users.noreply.github.com>
2026-04-14 16:55:49 -0300

    Implement initial Critter class with constructor
    Added a basic structure for the Critter class with a constructor and a Greet method.

---
+++ Book Demonstrations/POO/08 Classes: Critter Caretaker/constructor_critter.cpp
@@ -0,0 +1,25 @@
+// Constructor Critter
+// demonstrates constructors
+
+#include <iostream>
+using namespace std;
+
+class Critter 
+{
+
+}
+
+Critter::Critter(int hunger)
+{
+
+}
+
+void Critter::Greet()
+{
+  
+}
+
+int main()
+{
+  return 0;
+}

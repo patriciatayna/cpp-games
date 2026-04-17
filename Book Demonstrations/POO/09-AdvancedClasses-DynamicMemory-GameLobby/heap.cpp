ac5b59b18a1ac6adb091fca0bb61d4998e25c330
Patrícia Tayná <69331672+patriciatayna@users.noreply.github.com>
2026-04-16 21:43:59 -0300

    Add heap.cpp for posterior dynamic memory allocation example
    basic structure for heap memory demonstration.

---
+++ Book Demonstrations/POO/09-AdvancedClasses-DynamicMemory-GameLobby/heap.cpp
@@ -0,0 +1,29 @@
+// Heap
+// Demonstrates dynamically allocating memory
+
+#include <iostream>
+using namespace std;
+
+int* intOnHeap(); // returns an int on the heap
+void leak1(); // creates memory leak
+void leak2(); // creates another memory leak
+
+int main()
+{
+
+}
+
+int* intOnHeap()
+{
+  
+}
+
+void leak1()
+{
+
+}
+
+void leak2()
+{
+
+}

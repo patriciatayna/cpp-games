25f467c57a51f1fdadeef43c282e805c136ed468
Patrícia Tayná <69331672+patriciatayna@users.noreply.github.com>
2026-04-16 22:13:45 -0300

    Add play again feature using do-while loop

---
+++ Book Demonstrations/02_Guess_My_Number/05_play_again2.cpp
@@ -0,0 +1,19 @@
+// Play Again
+// Demonstrates do-while loops
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char again;
+    do
+    {
+        cout << "\n**Played an exciting game**";
+        cout << "\nDo you want to play again? (y/n): ";
+        cin >> again;
+    } while (again == 'y');
+
+    cout << "\nOkay, bye.";
+    return 0;
+}
